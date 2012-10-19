645839ac1b94402d7b74a8d9defbc69c6598bbed
Yoann Basset <yoann@madebykawet.com>
2012-10-19 20:10:56 +0200

    working

--- library/src/com/actionbarsherlock/app/SherlockListActivity.java
+++ library/src/com/actionbarsherlock/app/SherlockListActivity.java
@@ -1,12 +1,12 @@
 package com.actionbarsherlock.app;
 
-import android.app.ListActivity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.Window;
 import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+
 import com.actionbarsherlock.ActionBarSherlock;
 import com.actionbarsherlock.ActionBarSherlock.OnActionModeFinishedListener;
 import com.actionbarsherlock.ActionBarSherlock.OnActionModeStartedListener;
@@ -17,6 +17,7 @@ import com.actionbarsherlock.view.ActionMode;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
+import com.good.gd.ListActivity;
 
 public abstract class SherlockListActivity extends ListActivity implements OnCreatePanelMenuListener, OnPreparePanelListener, OnMenuItemSelectedListener, OnActionModeStartedListener, OnActionModeFinishedListener {
     private ActionBarSherlock mSherlock;
